6d1c071403497cd3974b9b270a02b53f72f97fe9
Selesterkft <43983808+Selesterkft@users.noreply.github.com>
2018-12-05 20:40:09 +0100

    igy jobb?

--- app/src/main/java/com/example/selester/dkdprototype/Activityb.java
+++ app/src/main/java/com/example/selester/dkdprototype/Activityb.java
@@ -1,7 +1,10 @@
 package com.example.selester.dkdprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activityb extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Activityb extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activityb);
+
+
+        Button button = (Button) findViewById(R.id.backgroundbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityc();
+            }
+        });
     }
-}
+    public void openActivityc(){
+        Intent intent = new Intent(this,Activityc.class);
+        startActivity(intent);
+    }
+    }
--- app/src/main/java/com/example/selester/dkdprototype/Activityc.java
+++ app/src/main/java/com/example/selester/dkdprototype/Activityc.java
@@ -1,11 +1,13 @@
 package com.example.selester.dkdprototype;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Activityc extends AppCompatActivity {
 
@@ -13,17 +15,19 @@ public class Activityc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activityc);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        Button button = (Button) findViewById(R.id.backgroundbutton2);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                openActivityd();
             }
         });
     }
+    public void openActivityd(){
+        Intent intent = new Intent(this,Activityd.class);
+        startActivity(intent);
+            }
 
 }
