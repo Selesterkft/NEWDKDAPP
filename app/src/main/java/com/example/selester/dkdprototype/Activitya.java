1b12b34cc1ca477bbb9d9584a7f24437aba75af9
Mészáros Attila Gábor <meszarosag@gmail.com>
2018-12-19 12:32:45 +0100

    Attila változat

--- app/src/main/java/com/example/selester/dkdprototype/Activitya.java
+++ app/src/main/java/com/example/selester/dkdprototype/Activitya.java
@@ -20,49 +20,42 @@ public class Activitya extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
 
-        b1 = (Button) findViewById(R.id.loginbutton);
-        ed1 = (EditText) findViewById(R.id.editText1);
-        ed2 = (EditText) findViewById(R.id.editText2);
-        final EditText ed3 = (EditText) findViewById(R.id.editText3);
+        b1  = findViewById(R.id.loginbutton);
+        ed1 = findViewById(R.id.editText1);
+        ed2 = findViewById(R.id.editText2);
+        final EditText ed3 = findViewById(R.id.editText3);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ed1.getText().toString().equals("admin") &&
-                        ed2.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(),
-                            "Succsess", Toast.LENGTH_SHORT).show();
-                    openActivityb();
+            if (ed1.getText().toString().equals("admin") &&
+                    ed2.getText().toString().equals("admin")) {
+                Toast.makeText(getApplicationContext(),
+                        "Succsess", Toast.LENGTH_SHORT).show();
+                openActivityb();
 
-                }else if (ed3.getText().toString().equals("0000")) {
-                    openactivityc();
-                    Toast.makeText(getApplicationContext(),
-                            "Inside", Toast.LENGTH_SHORT).show();
+            }else if (ed3.getText().toString().equals("0000")) {
+                openactivityc();
+                Toast.makeText(getApplicationContext(),
+                        "Inside", Toast.LENGTH_SHORT).show();
 
-                }
-
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-
-                }
-
-
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
             }
-            });
-
         }
-        public void openActivityb ()
-        {
-            Intent intent = new Intent(this, Activityb.class);
-            startActivity(intent);
-
-        }
-
-        public void openactivityc ()
-        {
-            Intent intent2 = new Intent(this, Activityc.class);
-            startActivity(intent2);
-        }
-
+        });
     }
+    public void openActivityb () {
+        Bundle b = new Bundle();
+        b.putString("elso","ez az");
+        Intent intent = new Intent(this, Activityb.class);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    public void openactivityc () {
+        Intent intent2 = new Intent(this, Activityc.class);
+        startActivity(intent2);
+    }
+
+}
